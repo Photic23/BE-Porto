7bff39f091216257be1042f0ce5bdc6bb18e836b
Photic23 <naufalmahdy66@gmail.com>
2024-05-26 15:31:38 +0700

    adding temp landing page endpoint

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("API is working properly");
+    res.send({'textIndo': 'Halo, saya Photic', 'textEnglish': 'Hello, I am Photic', 'textDeutsch': 'Hallo, ich bin Photic', 'textJapan':'こんにちは、私はフォトです'});
 });
 
 module.exports = router;
